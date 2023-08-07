ef59dd7035502de9024d5971dd91b6beb36bc7ff
nicolasenciso <nricardoe@unal.edu.co>
2023-08-07 00:34:01 -0500

    basic table ready

--- snake.cpp
+++ snake.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
+
+#define print(x) std::cout<<x<<std::endl;
+
 void startGame();
 char** fillInitialTable(int* rows, int* columns);
 
 int main(){
 
-    std::cout<<"WELCOME"<<std::endl;
-    std::cout<<"PRESS ANY LETTER AND ENTER BEGIN"<<std::endl;
+    print("WELCOME");
+    print("PRESS ANY LETTER AND ENTER BEGIN");
     
     char beginButton;
     std::cin>>beginButton;
@@ -15,36 +18,37 @@ int main(){
         system("clear");
         startGame();
     }else{
-        std::cout<<"ADIOS"<<std::endl;
+        print("ADIOS");
     }
 
 }
 
 void startGame(){
     int rows = 20;
-    int columns = 40;
+    int columns = 60;
     
     char** table =  fillInitialTable(&rows, &columns);
     for(int i = 0; i < rows; i++){
-        std::cout<<table[i]<<std::endl;
-        for(int j = 0; j < columns; j++){
-            
-        }
+        print(table[i]);
     }
 
 }
 
 char** fillInitialTable(int* rows, int* columns){
     
-    char** table = new char *[(*rows)];
+    char** table = new char* [(*rows)];
     
     for(int i = 0; i < *rows; i++){
         table[i] = new char[(*columns)];
         for(int j = 0; j < *columns; j++){
-            table[i][j] = 'A';
+            if(i == 0 || i == *rows - 1 || j == 0 || j == *columns - 1){
+                table[i][j] = 'W';
+            }else{
+                table[i][j] = '_';
+            }
+            
         }
     }
 
     return table;
-
 }
